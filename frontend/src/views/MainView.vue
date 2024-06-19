52da8d4c2918beba918b6a2ee0d995377b79c6b8
VigularIgnat <ignatvigular@gmail.com>
2024-06-19 21:10:18 +0200

    Adding login view and tailwindcss to frontend

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+    // const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+    import { vMaska } from "maska/vue";
+    import { computed, onMounted, reactive, ref} from "vue";
+    import axios from "axios";
+    import { useRouter } from "vue-router";
+
+    const router = useRouter()
+
+    // const credentials = reactive({
+    //     phone:null
+    // })
+
+    
+    // const waitingOnVerification = ref(false)
+
+    onMounted(() => {
+        if(localStorage.getItem('token')){
+            router.push({
+                name: 'main'
+            })
+        }
+    })
+ 
+</script>
+
+<template>
+    
+</template>
